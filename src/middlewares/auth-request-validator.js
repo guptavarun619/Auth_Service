ef422aabe0d53e2c7e8b02afcfa9e10db2d785ab
guptavarun619 <guptavarun619@gmail.com>
2022-12-22 14:23:23 +0530

    [Feat] Added middleware for email, password validation for incoming request

---
+++ src/middlewares/auth-request-validator.js
@@ -0,0 +1,15 @@
+const validateUserAuth = (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({
+      success: false,
+      data: {},
+      message: "Something went wrong in authentication",
+      err: "Email or password is missing in the authentication request",
+    });
+  }
+  next();
+};
+
+module.exports = {
+  validateUserAuth,
+};
